19a53b6faac9187e000e648e4d755f09a5c111e3
Varinder D <varinderd@hotmail.com>
2017-06-04 15:12:21 -0400

    Add files via upload

--- mp4-to-mp3.py
+++ mp4-to-mp3.py
@@ -1,4 +1,18 @@
-# MP4 to MP3 Convertor
+'''
+
+    By: Varinder
+	
+
+    Python3, ffmpeg, lame
+    Convert mp4 video file to mp3 audio track using ffmpeg and lame
+
+	Put this file in the drectory where you have all the mp4 videos and it will 
+	generate mp3 files for all of them one at a time
+
+	Example: 
+	# python mp4-to-mp3.py
+
+'''
 
 import os
 import fnmatch
@@ -11,11 +25,9 @@ for file in os.listdir('.'):
         mp4list.append(file)
 
 for mp4 in mp4list:
-	# print("Processing", mp4, "...", end="")
-	print("Processing", mp4 + "... ")
+	print("Processing", mp4)
 	mp3 = mp4[0:-1] + '3'
 	ffmpeg_cmd = 'ffmpeg -i "' + mp4 + '" -vn -acodec libmp3lame -ac 2 -qscale:a 4 -ar 48000 "' + mp3 + '"'
 	print(ffmpeg_cmd)
 	subprocess.call(ffmpeg_cmd)
-	# print(proc.communicate())
 
